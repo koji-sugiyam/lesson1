6e7a55a044e50d4c03f598743840e35137fd1843
STARTTODAY\koji.sugiyama <koji.sugiyama@starttoday.jp>
2017-12-12 09:34:44 +0900

    Add @RequestParamの構文追加

--- spring-boot-starter-parent/src/main/java/lesson2/getParamController.java
+++ spring-boot-starter-parent/src/main/java/lesson2/getParamController.java
@@ -17,22 +17,35 @@ public class getParamController {
     }
 
   
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/sugiyama", method = RequestMethod.GET)
     @ResponseBody
     public String home(
-    		@RequestParam(required = true) int id,
-    		@RequestParam(required = true) long uuid,
-    		@RequestParam(required = false) double score,
-    		@RequestParam(required = false) String name
+    		@RequestParam(name = "id", required = true) Integer id,
+    		@RequestParam(name = "uuid", required = true) Long uuid,
+    		@RequestParam(name = "score", required = false, defaultValue = "") Double score,
+    		@RequestParam(name = "name", required = false, defaultValue = "") String name
     		) 
     {
-    	String data; 
-    	data = "id = " + id +" ";
-    	data += "uuid = " + uuid + " ";
-    	data += "score = " + score;
+    	String data = ""; 
+
+    	/*
+		data += "id = " + id.toString();
+		data += "uuid = " + uuid.toString();
+		data += "score = " + score.toString();
+		data += "name = " + name;
+    	*/
     	
-    	if(!name.isEmpty()) {
-    		data += "name = " + name;
+    	if (id != null) {
+    		data += " id = " + id.toString();
+    	}
+    	if (uuid != null) {
+    		data += " uuid = " + uuid.toString();
+    	}
+    	if (score != null) {
+    		data += " score = " + score.toString();
+    	}
+    	if(name != null) {
+    		data += " name = " + name;
     	}
     	
         return data;
