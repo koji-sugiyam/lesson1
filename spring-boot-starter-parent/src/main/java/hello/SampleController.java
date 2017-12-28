1ec591d54a0f47e763852df4f5769d42017cc722
STARTTODAY\koji.sugiyama <koji.sugiyama@starttoday.jp>
2017-12-28 14:10:56 +0900

    Add Hello World

--- spring-boot-starter-parent/src/main/java/hello/SampleController.java
+++ spring-boot-starter-parent/src/main/java/hello/SampleController.java
@@ -6,16 +6,15 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.Scanner;
-
-
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-    private static final int LocalDateTime = 0;
+    //springで実行する場合
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
 
 	@RequestMapping("/")
     @ResponseBody
@@ -23,9 +22,5 @@ public class SampleController {
         return "Hello World!";
     }
     
-    //springで実行する場合
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
-    }
-
+	
 }
